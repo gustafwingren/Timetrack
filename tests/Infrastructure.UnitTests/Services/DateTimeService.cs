a51ff51c5542d024a6e4f0feba1c4cf6a160170a
gustafwingren <gustaf@gwingren.se>
2022-04-10 16:15:17 +0200

    WIP

---
+++ tests/Infrastructure.UnitTests/Services/DateTimeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace Infrastructure.UnitTests.Services;
+
+public class DateTimeService
+{
+    private readonly Infrastructure.Services.DateTimeService _sut;
+    public DateTimeService()
+    {
+        _sut = new Infrastructure.Services.DateTimeService();
+    }
+
+    [Fact]
+    public void Now_ShouldReturnDateTimeNow()
+    {
+        // Act
+        var now = _sut.Now;
+
+        // Assert
+        Assert.Equal(DateTime.Now, now);
+    }
+
+    [Fact]
+    public void Today_ShouldReturnDatetimeToday()
+    {
+        // Act
+        var today = _sut.Today;
+
+        // Assert
+        Assert.Equal(DateOnly.FromDateTime(DateTime.Today), today);
+    }
+}
